c659d6894adf3c36929e1474467bf60774ea0acc
Ben <ben@reticent-monlith.com>
2021-07-15 12:28:12 +0100

    time fix

--- src/models/Dispatch.js
+++ src/models/Dispatch.js
@@ -1,7 +1,7 @@
 export default class Dispatch {
     constructor(dispatchState) {
-        this.date = dispatchState.date || new Date().toLocaleDateString
-        this.time = dispatchState.time || new Date().toLocaleTimeString
+        this.date = dispatchState.date || new Date().toLocaleDateString()
+        this.time = dispatchState.time || new Date().toLocaleTimeString()
         this.riders = dispatchState.riders
         this.windSpeed = dispatchState.windSpeed
         this.windDegrees = dispatchState.windDegrees
